e11c0e64e7f9018fbc56995ccf219e0decebca4d
dgarciaCl <danig.ocana@gmail.com>
2025-12-01 12:10:06 +0100

    Solution to part #2

---
+++ Day 1 Secret Entrance/Part 1.py
@@ -0,0 +1,25 @@
+vfid = open("input.txt")
+
+pwd = 0
+dial = 50
+
+#Loop through the sequence of rotations
+for rotation in vfid:
+    #Extract the direction and the distance of the rotation
+    dir = rotation[0]
+    dist = int(rotation[1:-1])
+
+    #Move the dial left or right depending on the direction
+    if dir == 'R':
+        dial += dist
+    else:
+        dial -= dist
+
+    #Use the modulus to update the number the dial points to
+    dial %= 100
+
+    #Count the number of times the dial is left pointing at 0
+    if dial == 0:
+        pwd += 1
+
+print("The actual password to open the door is", pwd)
---
+++ Day 1 Secret Entrance/Part 2.py
@@ -0,0 +1,26 @@
+vfid = open("input.txt")
+
+pwd = 0
+dial = 50
+
+#Loop through the sequence of rotations
+for rotation in vfid:
+    #Extract the direction and the distance of the rotation
+    dir = rotation[0]
+    dist = int(rotation[1:-1])
+
+    #Move the dial left or right depending on the direction
+    if dir == 'R':
+        dial += dist
+    else:
+        dial -= dist
+    
+    #Count the number of times the dial points at 0 throughout the rotation
+    rem = dial % 100
+    zeros = abs((dial - rem) / 100)
+
+    #Update the password and the number the dial points to
+    pwd += zeros
+    dial = rem
+
+print("The password to open the door is", int(pwd))
