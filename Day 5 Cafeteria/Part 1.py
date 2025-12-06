6d5eac7011eee01e8770044091178a2156386960
dgarciaCl <danig.ocana@gmail.com>
2025-12-06 22:02:38 +0100

    A bit late but here are the solutions for day 5

---
+++ Day 5 Cafeteria/Part 1.py
@@ -0,0 +1,44 @@
+vfid = open("input.txt")
+
+ranges = []
+IDs = []
+
+blankline = False
+
+# Read through the database and append the line into the correct list
+for line in vfid:
+    # The empty line separates the ranges from the IDs
+    if line == "\n":
+        blankline = True
+    # If the blank line has not been reached, we append the line into our ingredient ID ranges
+    elif not blankline:
+        dash = line.find("-")
+
+        # We find the start and the end of our range
+        start = int(line[0 : dash])
+        end = int(line[dash + 1 : -1])
+
+        ranges.append((start, end))
+    # If the blank line has been reached, we append the line into our ingredient IDs
+    else:
+        IDs.append(int(line[: -1]))
+
+fresh = 0
+
+# Loop through our ingredient IDs
+for ingredient in IDs:
+    spoilt = True
+
+    # Check for each of our ingredient ID ranges
+    for start, end in ranges:
+        # If the ingredient ID is inside any ID range, then it is fresh
+        if (start <= ingredient) and (ingredient <= end):
+            spoilt = False
+        
+    # If an ingredient ID is fresh, then we take note of it
+    if not spoilt:
+        fresh += 1
+
+print(fresh, "of the available ingredient IDs are fresh")
+
+vfid.close()
---
+++ Day 5 Cafeteria/Part 2.py
@@ -0,0 +1,43 @@
+vfid = open("input.txt")
+
+ranges = []
+
+blankline = False
+
+# Read through the database and append the line into the correct list
+for line in vfid:
+    # The empty line separates the ranges from the IDs
+    if line == "\n":
+        blankline = True
+    # If the blank line has not been reached, we append the line into our ingredient ID ranges
+    elif not blankline:
+        dash = line.find("-")
+
+        # We find the start and the end of our range
+        start = int(line[0 : dash])
+        end = int(line[dash + 1 : -1])
+
+        ranges.append((start, end))
+
+# Sort our igredient ID ranges to find the fresh ingredient ID ranges
+ranges.sort()
+IDs = []
+
+# Loop through our igredient ID ranges
+for start, end in ranges:
+    # If our range doesnt overlap with the previous one, we append it
+    if (len(IDs) == 0) or (IDs[-1][1] < start):
+        IDs.append([start, end])  
+    # If our range overlaps with the previous one, we modify it by taking their union
+    else:
+        IDs[-1][1] = max(IDs[-1][1], end)
+
+fresh = 0
+
+# Loop through our fresh ingredient ID ranges to find how many ingredient IDs are considered to be fresh
+for start, end in IDs:
+    fresh += (end + 1) - start
+
+print("According to the fresh ingredient ID ranges,", fresh, "ingredient IDs are considered to be fresh")
+
+vfid.close()
